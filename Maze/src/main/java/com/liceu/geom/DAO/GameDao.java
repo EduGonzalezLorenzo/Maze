4867041d757efe8a09e770c86d8f4402cdb75254
Edu <edugonzalezlorenzo@gmail.com>
2022-11-15 20:10:14 +0100

    Cambios varios

--- Maze/src/main/java/com/liceu/geom/DAO/GameDao.java
+++
@@ -1,9 +0,0 @@
-package com.liceu.geom.DAO;
-
-import com.liceu.geom.model.Game;
-import com.liceu.geom.model.MazeMap;
-import com.liceu.geom.model.Player;
-
-public interface GameDao {
-    Game newGame(Player player, MazeMap mazeMap);
-}
--- Maze/src/main/java/com/liceu/geom/controllers/StartController.java
+++ Maze/src/main/java/com/liceu/geom/controllers/StartController.java
@@ -3,28 +3,32 @@ package com.liceu.geom.controllers;
 import com.liceu.geom.model.Game;
 import com.liceu.geom.model.MazeMap;
 import com.liceu.geom.services.GameService;
+import com.mysql.cj.Session;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebServlet("/select")
+@WebServlet("/start")
 public class selectLevelController extends HttpServlet {
     GameService gameService = new GameService();
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/jsp/selectLevel.jsp");
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/jsp/start.jsp");
         dispatcher.forward(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        int mazeMapType = Integer.parseInt(req.getParameter("mazeMap"));
-        //Game game = gameService.createNewGame(mazeMapType);
-        req.setAttribute("game",mazeMapType);
+        int mapId = Integer.parseInt(req.getParameter("mazeMap"));
+        Game game = gameService.createNewGame(mapId);
+        HttpSession session = req.getSession();
+        session.setAttribute("game", game);
         RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/jsp/game.jsp");
         dispatcher.forward(req, resp);
     }
