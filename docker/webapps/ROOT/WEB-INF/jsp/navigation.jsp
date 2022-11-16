413bed6c6f8acd76060711041f32acf6e7142684
Edu <edugonzalezlorenzo@gmail.com>
2022-11-16 19:12:54 +0100

    Mejoras graficas.

--- docker/webapps/ROOT/WEB-INF/jsp/navigation.jsp
+++ docker/webapps/ROOT/WEB-INF/jsp/navigation.jsp
@@ -1,7 +1,7 @@
 <%@ page isELIgnored="false" %>
 <%@ taglib uri ="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
-<%-- <style><%@include file="/styles.css"%></style> --%>
+<style><%@include file="/css/styles.css"%></style>
 
 <!DOCTYPE html>
 <html lang="es">
@@ -10,7 +10,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <%-- <link rel="stylesheet" href="styles.css"> --%>
+    <link rel="stylesheet" href="/css/styles.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
     <title>MazmorraJuego</title>
@@ -19,7 +19,7 @@
 <body class="text-center bg-primary" onload="drawCanvas();">
     <h1>Mazmorra Juego</h1>
     <article class="bg-light justify-content-center">
-            <canvas id="canvas" width="800" height="600" style="border:1px solid #000000;"></canvas>
+            <canvas id="canvas" width="800" height="610" style="border:1px solid #000000;"></canvas>
     </article>
     <script type="application/json" id="gameJson">
         ${gameJson}
--- docker/webapps/ROOT/js/canvas.js
+++ docker/webapps/ROOT/js/canvas.js
@@ -1,6 +1,7 @@
 //El canvas sobre el que se dibujará el tablero
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
+ctx.font = "20px gameFont";
 
 let columns = 20;
 let rows = 15;
@@ -13,16 +14,35 @@ const botArrow = new Image();
 const righttArrow = new Image();
 const leftArrow = new Image();
 
+const wallHor = new Image();
+const wallVer = new Image();
+const DoorOpenHor = new Image();
+const DoorOpenVer = new Image();
+const DoorClosedHor = new Image();
+const DoorClosedVer = new Image();
 
 topArrow.src = "../img/Arrow_Up.png";
 leftArrow.src = "../img/Arrow_Left.png";
 botArrow.src = "../img/Arrow_Down.png";
 righttArrow.src = "../img/Arrow_Right.png";
 
+wallHor.src = "../img/MuroHorizontal.png";
+wallVer.src = "../img/MuroVertical.png";
+DoorOpenHor.src = "../img/PuertaAbiertaHorizontal.png";
+DoorOpenVer.src = "../img/PuertaAbiertaVertical.png";
+DoorClosedHor.src = "../img/PuertaCerradaHorizontal.png";
+DoorClosedVer.src = "../img/PuertaCerradaVertical.png";
 
 function drawCanvas() {
     drawArrows();
-    drawCurrentRoom();
+    let dataScript = document.getElementById("gameJson").textContent;
+    let data = JSON.parse(dataScript);
+    let room = new Object();
+    let player = new Object();
+    room = data.Room;
+    player = data.Player;
+    drawCurrentRoom(room);
+    drawCurrentInventory(player);
 }
 
 function drawArrows() {
@@ -32,15 +52,52 @@ function drawArrows() {
     ctx.drawImage(righttArrow, 740, 540, 50, 50);
 }
 
-function drawCurrentRoom(){
-    let dataScript = document.getElementById("gameJson").textContent;
-    let data = JSON.parse(dataScript);
-    let room = new Object();
-    let player = new Object();
-    room = data.Room;
-    player = data.Player;
-    console.log(room);
-    console.log(player);
+
+function drawCurrentInventory(player){
+    ctx.fillText("Coins", 650, 200);
+    ctx.fillText(player.Coins, 680, 225);
+    ctx.fillText("Keys", 650, 275);
+    ctx.fillText(player.Keys, 680, 300);
+}
+
+function drawCurrentRoom(room) {
+    drawSideHor(room.N, 10, 10, 600, 100);
+    drawSideVer(room.W, 10, 50, 100, 500);
+    drawSideHor(room.S, 10, 500, 600, 100);
+    drawSideVer(room.E, 510, 50, 100, 500);
+    ctx.fillText("Room", 650, 100);
+    ctx.fillText(room.roomNumber, 680, 125);
+}
+
+function drawSideHor(side, x, y, width, height){
+    switch (side) {
+        case "wall":
+            ctx.drawImage(wallHor, x, y, width, height);
+            break;
+        case "Open door":
+            ctx.drawImage(DoorOpenHor, x, y, width, height);
+            break;
+        case "Close door":
+            ctx.drawImage(DoorClosedHor, x, y, width, height);
+    }
+}
+
+function drawSideVer(side, x, y, width, height){
+    switch (side) {
+        case "wall":
+            ctx.drawImage(wallVer, x, y, width, height);
+            break;
+        case "Open door":
+            ctx.drawImage(DoorOpenVer, x, y, width, height);
+            break;
+        case "Close door":
+            ctx.drawImage(DoorClosedVer, x, y, width, height);
+    }
+}
+
+function drawPlayerItems(player) {
+    console.log("Coins: " + player.Coins);
+    console.log("Keys: " + player.Keys);
 }
 
 canvas.addEventListener("mousedown", function (event) {
